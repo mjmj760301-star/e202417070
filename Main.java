f909e41c61bdcf79e4b44aa16606a0ac585e906d
Choi, Eunso <eunso1190@gmail.com>
2025-09-17 17:05:17 +0900

    one

--- Main.java
+++ Main.java
@@ -1,8 +1,6 @@
-public class Main{
+public class Main {
 
     public static void main(String[] args){
-        System.out.prinln("박규민");
-        System.out.println("안녕하세요");
-        System.out.println("hello world");
+        System.out.println("박규민");
     }
 }
